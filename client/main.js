f3bf44b9a99de5c093bc887da14b8e72978a2b6e
Kavita Baradur <kavitabaradur@gmail.com>
2018-08-12 00:08:05 -0400

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Resolutions = new Mongo.Collection('resolutions');
+
+Template.body.helpers({
+  resolutions: function(){
+  	return Resolutions.find();
+  }
+});
